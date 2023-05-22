720f41a91073587f55a809ce4fb5af58daae53b8
TaseenFuadJeem <mdtaseenfuad@gmail.com>
2023-05-22 12:36:54 +0600

    Added custom fonts

--- src/Resources/components/NavigationBar.jsx
+++ src/Resources/components/NavigationBar.jsx
@@ -13,7 +13,7 @@ const NavigationBar = () => {
         className="relative flex w-full flex-nowrap items-center justify-between bg-neutral-100 py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:flex-wrap lg:justify-start lg:py-4"
         data-te-navbar-ref=""
       >
-        <div className="flex w-full flex-wrap items-center justify-between px-3">
+        <div className="flex w-full mx-auto max-w-screen-xl flex-wrap items-center justify-between px-3">
           {/* Hamburger button for mobile view */}
           <button
             className="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden"
@@ -55,7 +55,7 @@ const NavigationBar = () => {
             </a>
             {/* Left links */}
             <ul
-              className="list-style-none mr-auto flex flex-col pl-0 lg:flex-row"
+              className="list-style-none ml-auto flex flex-col pl-0 lg:flex-row"
               data-te-navbar-nav-ref=""
             >
               {/* Home link */}
@@ -69,30 +69,37 @@ const NavigationBar = () => {
                   Home
                 </a>
               </li>
-              {/* Features link */}
-              <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref="">
+              {/* About link */}
+              <li className="my-4 lg:my-0 lg:pr-2" data-te-nav-item-ref="">
                 <a
-                  className="p-0 text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                  className="active disabled:text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                  aria-current="page"
                   href="#"
                   data-te-nav-link-ref=""
                 >
-                  Features
+                  About
                 </a>
               </li>
-              {/* Pricing link */}
-              <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref="">
+              {/* Properties link */}
+              <li className="my-4 lg:my-0 lg:pr-2" data-te-nav-item-ref="">
                 <a
-                  className="p-0 text-neutral-500 hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                  className="active disabled:text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                  aria-current="page"
                   href="#"
                   data-te-nav-link-ref=""
                 >
-                  Pricing
+                  Properties
                 </a>
               </li>
-              {/* Disabled link */}
-              <li className="lg:pr-2" data-te-nav-link-ref="">
-                <a className="text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400">
-                  Disabled
+              {/* Contact link */}
+              <li className="my-4 lg:my-0 lg:pr-2" data-te-nav-item-ref="">
+                <a
+                  className="active disabled:text-black/30 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                  aria-current="page"
+                  href="#"
+                  data-te-nav-link-ref=""
+                >
+                  Contact
                 </a>
               </li>
             </ul>
