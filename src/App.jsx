7cd9c741b5fe3fa1664f5cc02bc6416ac62a9824
TaseenFuadJeem <mdtaseenfuad@gmail.com>
2023-05-23 15:05:46 +0600

    Added whatsapp api

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "./Resources/pages/About";
 import Properties from "./Resources/pages/Properties";
 import Contact from "./Resources/pages/Contact";
 import Footer from "./Resources/components/Footer";
+import whatsappicon from "./Resources/assets/icons/whatsapp.png";
 
 function App() {
   return (
@@ -13,6 +14,13 @@ function App() {
       className="overflow-hidden bg-no-repeat bg-cover bg-fixed"
       style={{ backgroundImage: `url(${bg})` }}
     >
+      <a href="https://wa.me/+971555147420?text=">
+        <img
+          src={whatsappicon}
+          className="lg:w-16 w-12 fixed bottom-10 right-10 z-50"
+          alt="whatsappicon"
+        />
+      </a>
       <NavigationBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
