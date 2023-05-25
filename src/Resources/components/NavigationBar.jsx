17f2fc31e0bfda4630fea5d1aca5272a10b1ab1f
TaseenFuadJeem <mdtaseenfuad@gmail.com>
2023-05-25 13:33:06 +0600

    Fixed the navbar issue

--- src/Resources/components/NavigationBar.jsx
+++ src/Resources/components/NavigationBar.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import logo from "../assets/icons/300.png";
 import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleMenuItemClick = () => {
+    setIsOpen(false);
+    document.getElementById("dropdown-button").focus();
+  };
+
   return (
     <nav className="bg-white fixed w-full top-0 z-50 shadow-lg">
       <div className="navbar mx-auto max-w-7xl">
@@ -27,7 +39,11 @@ const NavigationBar = () => {
           </ul>
         </div>
         <div className="dropdown dropdown-end ml-auto">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+          <label
+            tabIndex={0}
+            onClick={handleClick}
+            className="btn btn-ghost lg:hidden"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -45,18 +61,20 @@ const NavigationBar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-compact dropdown-content mt-5 p-2 shadow bg-base-100 rounded-box w-52"
+            className={`menu menu-compact dropdown-content mt-5 p-2 shadow bg-base-100 rounded-box w-52 ${
+              isOpen ? "block" : "hidden"
+            }`}
           >
-            <li className="mx-2">
+            <li className="mx-2" onClick={handleMenuItemClick}>
               <Link to="/home">Home</Link>
             </li>
-            <li className="mx-2">
+            <li className="mx-2" onClick={handleMenuItemClick}>
               <Link to="/about-us">About</Link>
             </li>
-            <li className="mx-2">
+            <li className="mx-2" onClick={handleMenuItemClick}>
               <Link to="/properties">Properties</Link>
             </li>
-            <li className="mx-2">
+            <li className="mx-2" onClick={handleMenuItemClick}>
               <Link to="/contact-us">Contact</Link>
             </li>
           </ul>
