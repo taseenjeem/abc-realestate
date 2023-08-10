b7a90f465d59a8d816363a4b6d39b84fb258becc
TaseenFuadJeem <mdtaseenfuad@gmail.com>
2023-08-10 14:18:18 +0600

    Added modal

--- src/Resources/components/NavigationBar.jsx
+++ src/Resources/components/NavigationBar.jsx
@@ -57,12 +57,53 @@ const NavigationBar = () => {
                 Get Consultancy
               </button>
               <Modal open={open} onClose={onCloseModal} center>
-                <h2>Simple centered modal</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                <h2 className="text-3xl font-semibold">How can we help you?</h2>
+                <p className="my-3">
+                  If you have any queries for us, please do not hesitate to ask.
+                  Please feel free to contact us using the following form.
                 </p>
+                <div className="divider"></div>
+                <form action="">
+                  <div className="md:flex md:gap-5">
+                    <div className="form-control w-full max-w-lg mx-auto">
+                      <label className="label">
+                        <span className="label-text">Your Name</span>
+                      </label>
+                      <input
+                        type="text"
+                        required
+                        placeholder="Type your name"
+                        className="input input-primary input-bordered w-full max-w-lg"
+                      />
+                    </div>
+                    <div className="form-control w-full max-w-lg mx-auto">
+                      <label className="label">
+                        <span className="label-text">Your Email</span>
+                      </label>
+                      <input
+                        type="email"
+                        required
+                        placeholder="Type your email address"
+                        className="input input-primary input-bordered w-full max-w-lg"
+                      />
+                    </div>
+                  </div>
+                  <div className="form-control mt-5">
+                    <label className="label">
+                      <span className="label-text">Your Massage</span>
+                    </label>
+                    <textarea
+                      required
+                      className="textarea textarea-primary textarea-bordered h-24"
+                      placeholder="Type your massage"
+                    ></textarea>
+                  </div>
+                  <input
+                    type="submit"
+                    className="btn btn-primary capitalize mt-5 w-full"
+                    value="Send Massage"
+                  />
+                </form>
               </Modal>
             </div>
           </div>
